22d919e290ff1d3cc5a9700658507ccfc7690476
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-07-12 19:17:04 +0300

    init commit

---
+++ app/src/main/java/com/pancake/tictactoe/ui/screens/join/JoinScreen.kt
@@ -0,0 +1,15 @@
+package com.pancake.tictactoe.ui.screens.join
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun JoinScreen() {
+
+}
+
+@Composable
+private fun JoinContent() {
+
+}
