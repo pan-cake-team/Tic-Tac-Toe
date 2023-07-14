708be380701fb234f601fc61bc706ad7d506eeda
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-14 14:03:01 +0300

    update GameBoard

--- app/src/main/java/com/pancake/tictactoe/ui/navigation/MainNavigationGraph.kt
+++ app/src/main/java/com/pancake/tictactoe/ui/navigation/MainNavigationGraph.kt
@@ -3,19 +3,21 @@ package com.pancake.tictactoe.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import com.pancake.tictactoe.ui.screens.welcome.welcomeRoute
+import androidx.navigation.compose.composable
+import com.pancake.tictactoe.ui.screens.game.GameScreen
 
 @Composable
 fun MainNavigationGraph(navController: NavHostController) {
 
     NavHost(
         navController = navController,
-        startDestination = MainNavigationGraph.WELCOME_SCREEN
+        startDestination = MainNavigationGraph.GAME_SCREEN
     ) {
-        welcomeRoute(navController)
+        composable(MainNavigationGraph.GAME_SCREEN) { GameScreen() }
     }
 }
 
 object MainNavigationGraph {
     const val WELCOME_SCREEN = "welcomeScreen"
+    const val GAME_SCREEN = "gameScreen"
 }
--- app/src/main/java/com/pancake/tictactoe/ui/screens/game/composables/GameBoard.kt
+++ app/src/main/java/com/pancake/tictactoe/ui/screens/game/composables/GameBoard.kt
@@ -1,3 +1,5 @@
+@file:Suppress("OPT_IN_IS_NOT_ENABLED")
+
 package com.pancake.tictactoe.ui.screens.game.composables
 
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -10,49 +12,60 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.pancake.tictactoe.ui.screens.game.ButtonStatus
+import com.pancake.tictactoe.ui.screens.game.GameUiState
 import com.pancake.tictactoe.ui.theme.Gray
+import kotlinx.coroutines.flow.MutableStateFlow
 
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-@Preview(showBackground = true)
-fun GameBoard(){
+fun GameBoard(onClick: (Int) -> Unit, state: GameUiState) {
     LazyVerticalGrid(
         cells = GridCells.Fixed(3)
-    ){
-        items(count = 9){
-            GameBoardItem()
+    ) {
+        items(count = 9) {
+            val symbol = remember { MutableStateFlow(getTheSuitableSymbol(state,it)) }
+            symbol.value = getTheSuitableSymbol(state,it)
+            GameBoardItem(onClick = { onClick(it) }, symbol.value, state.isTurn)
         }
     }
 }
 
 @Composable
-@Preview(showBackground = true)
-fun GameBoardItem(){
-    var state by remember {
-        mutableStateOf(1)
-    }
+fun GameBoardItem(onClick: () -> Unit, symbol: String, enable: Boolean) {
     Box(
         modifier = Modifier
-            .border(
-                width = 1.dp,
-                color = Gray,
-            )
-            .padding(26.dp)
-            .clickable { state = 1 }
+            .border(width = 1.dp, color = Gray)
+            .clickable(onClick = onClick, enabled = enable)
+            .padding(26.dp),
     ) {
-        when(state){
-            0 -> Spacer(modifier = Modifier.size(56.dp))
-            1 -> Circle(modifier = Modifier.align(Alignment.Center))
-            2 -> Cross(modifier = Modifier.align(Alignment.Center))
+        when (symbol) {
+            "o" -> Circle(modifier = Modifier.align(Alignment.Center))
+            "x" -> Cross(modifier = Modifier.align(Alignment.Center))
+            else -> Spacer(modifier = Modifier.size(56.dp))
         }
     }
+}
+
+fun getTheSuitableSymbol(state: GameUiState, index: Int): String {
+    var symbol = ""
+    if (state.playerOne.buttonSelected == index) {
+        symbol = when (state.playerOne.buttonStatus) {
+            ButtonStatus.CROSS -> "x"
+            ButtonStatus.CIRCLE -> "o"
+            ButtonStatus.Empty -> ""
+        }
+    } else if (state.playerTwo.buttonSelected == index) {
+        symbol = when (state.playerTwo.buttonStatus) {
+            ButtonStatus.CROSS -> "x"
+            ButtonStatus.CIRCLE -> "o"
+            ButtonStatus.Empty -> ""
+        }
+    }
+    return symbol
 }
