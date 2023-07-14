12bd9fda46e106685a47b7bcc3757f0bb0e40926
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-14 14:02:41 +0300

    update viewModel & ui state

--- app/src/main/java/com/pancake/tictactoe/ui/screens/game/GameUiState.kt
+++ app/src/main/java/com/pancake/tictactoe/ui/screens/game/GameUiState.kt
@@ -1,5 +1,22 @@
 package com.pancake.tictactoe.ui.screens.game
 
 data class GameUiState(
+    val sessionId: String = "",
+    val isTurn: Boolean = true,
+    val playerOne: Player = Player(),
+    val playerTwo: Player = Player(),
     val isFinished: Boolean = false,
 )
+
+data class Player(
+    val name: String = "",
+    val score: Int = 0,
+    val buttonStatus: ButtonStatus = ButtonStatus.Empty,
+    val buttonSelected: Int = -1,
+)
+
+enum class ButtonStatus {
+    Empty,
+    CROSS,
+    CIRCLE
+}
--- app/src/main/java/com/pancake/tictactoe/ui/screens/game/GameViewModel.kt
+++ app/src/main/java/com/pancake/tictactoe/ui/screens/game/GameViewModel.kt
@@ -4,10 +4,20 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class GameViewModel @Inject constructor(): ViewModel() {
+class GameViewModel @Inject constructor() : ViewModel() {
     private val _state = MutableStateFlow(GameUiState())
     val state = _state.asStateFlow()
+
+
+    fun onClickGameBoard(index: Int) {
+        _state.update {
+            it.copy(
+                playerOne = Player(buttonStatus = ButtonStatus.CROSS, buttonSelected = index)
+            )
+        }
+    }
 }
