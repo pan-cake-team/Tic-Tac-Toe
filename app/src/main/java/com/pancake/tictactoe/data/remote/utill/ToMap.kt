44b614351274b98cd826b684a054814e875c7a12
ameeramjed <pr.ameer98@gmail.com>
2023-07-17 18:17:52 +0300

    add toMap extinction

--- app/src/main/java/com/pancake/tictactoe/data/remote/utill/ToMap.kt
+++ app/src/main/java/com/pancake/tictactoe/data/remote/utill/ToMap.kt
@@ -1,19 +1,5 @@
 package com.pancake.tictactoe.data.remote.utill
 
-import kotlin.reflect.KClass
-import kotlin.reflect.full.memberProperties
-
-fun <T : Any> toMap(obj: T): Map<String, Any?> {
-    return (obj::class as KClass<T>).memberProperties.associate { prop ->
-        prop.name to prop.get(obj)?.let { value ->
-            if (value::class.isData) {
-                toMap(value)
-            } else {
-                value
-            }
-        }
-    }
-}
 
 fun <T : Any> T.toMap(): Map<String, Any?> {
     return javaClass.declaredFields.associate { field ->
